e9b928be76a3be19ef8178bff582c268aa600980
Padman <frimpongy890@gmail.com>
2024-08-11 15:05:55 +0000

    modified home page buttons and some dialogs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:plant_disease_prediction/enums/plant_type.dart';
 import 'package:plant_disease_prediction/service/api_service.dart';
@@ -69,41 +71,49 @@ class _HomeState extends State<Home> {
       String confidence = '${(res.confidence * 100).toStringAsFixed(2)}%';
       showDialog(
           context: context,
-          builder: (_) => AlertDialog(
-                title: const Text('Results'),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                          text: 'Result: ',
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600),
-                          children: [
-                            TextSpan(
-                                text: res.className,
-                                style: const TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w400))
-                          ]),
-                    ),
-                    RichText(
-                      text: TextSpan(
-                          text: 'Confidence: ',
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600),
-                          children: [
-                            TextSpan(
-                                text: confidence,
-                                style: const TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w400))
-                          ]),
-                    )
-                  ],
+          builder: (_) => BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 10,
+                  sigmaY: 10,
+                ),
+                child: AlertDialog(
+                  title: const Text('Results'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                            text: 'Result: ',
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                            children: [
+                              TextSpan(
+                                  text: res.className,
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400))
+                            ]),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                            text: 'Confidence: ',
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                            children: [
+                              TextSpan(
+                                  text: confidence,
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400))
+                            ]),
+                      )
+                    ],
+                  ),
                 ),
               ));
       return;
@@ -118,24 +128,89 @@ class _HomeState extends State<Home> {
   void _onSelectImage() {
     showDialog(
         context: context,
-        builder: (_) => AlertDialog(
-              // title: const Text('SELECT'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextButton(
-                      onPressed: () {
+        builder: (_) => BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 2,
+                sigmaY: 2,
+              ),
+              child: AlertDialog(
+                // title: const Text('SELECT'),
+                content: Row(
+                  // mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    // TextButton(
+                    //     onPressed: () {
+                    //       Navigator.pop(context);
+                    //       onPickImage(ImageSource.gallery);
+                    //     },
+                    //     child: const Text('GALLERY')),
+                    GestureDetector(
+                      onTap: () {
                         Navigator.pop(context);
                         onPickImage(ImageSource.gallery);
                       },
-                      child: const Text('GALLERY')),
-                  TextButton(
-                      onPressed: () {
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.grey[300],
+                        ),
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        height: 60,
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/images/gallery.png',
+                              width: 70,
+                              height: 40,
+                            ),
+                            const Text(
+                              'GALLERY',
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
                         Navigator.pop(context);
                         onPickImage(ImageSource.camera);
                       },
-                      child: const Text('CAMERA'))
-                ],
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.grey[300],
+                        ),
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        height: 60,
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/images/camera.png',
+                              width: 70,
+                              height: 40,
+                            ),
+                            const Text(
+                              'CAMERA',
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // TextButton(
+                    //     onPressed: () {
+                    //       Navigator.pop(context);
+                    //       onPickImage(ImageSource.camera);
+                    //     },
+                    //     child: const Text('CAMERA'))
+                  ],
+                ),
               ),
             ));
   }
@@ -146,52 +221,97 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('PLANT DISEASE DETECTION'),
+        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButtonFormField<PlantType>(
-                hint: const Text('Select a plant type'),
-                value: _plantType,
-                items: PlantType.values
-                    .map((e) => DropdownMenuItem<PlantType>(
-                          value: e,
-                          child: Text(e.name),
-                        ))
-                    .toList(),
-                onChanged: onPlantTypeChanged),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: devSize.height * .3,
-              color: Colors.black12,
-              child: Center(
-                child: _image == null
-                    ? const Text('Please select an image')
-                    : Image.file(
-                        _image!,
-                        height: devSize.height * .3,
-                        width: double.infinity,
-                      ),
+      body: Container(
+        color: Color.fromARGB(255, 248, 248, 240),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownButtonFormField<PlantType>(
+                  hint: const Text('Select a plant type'),
+                  value: _plantType,
+                  items: PlantType.values
+                      .map((e) => DropdownMenuItem<PlantType>(
+                            value: e,
+                            child: Text(e.name),
+                          ))
+                      .toList(),
+                  onChanged: onPlantTypeChanged),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextButton(
-                onPressed: _onSelectImage, child: const Text('SELECT IMAGE')),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-                onPressed: _onPredict,
-                child: _isLoading
-                    ? const CircularProgressIndicator()
-                    : const Text('MAKE A PREDICTION'))
-          ],
+              Container(
+                height: devSize.height * .3,
+                // color: Colors.black12,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.4),
+                    ),
+                    color: Colors.white),
+                child: Center(
+                  child: _image == null
+                      ? const Text('Please select an image')
+                      : Container(
+                          height: devSize.height * 0.29,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Image.file(
+                            _image!,
+                            height: devSize.height * .3,
+                            width: double.infinity,
+                            // fit: BoxFit.cover,
+                          ),
+                        ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 37, 145, 127),
+                  // color: Color(0xFFA2B38B),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextButton(
+                    onPressed: _onSelectImage,
+                    child: const Text(
+                      'SELECT IMAGE',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  onPressed: _onPredict,
+                  child: _isLoading
+                      // ? const CircularProgressIndicator()
+                      ? const Text('Predicting...')
+                      : Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 15),
+                          decoration: BoxDecoration(
+                            // color: Color(0xFFA2B38B),
+                            color: Color.fromARGB(255, 37, 145, 127),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Text(
+                            'MAKE A PREDICTION',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ))
+            ],
+          ),
         ),
       ),
     );
