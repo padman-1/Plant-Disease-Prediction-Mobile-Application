0d7c14347abaf96ccebbfea97788dcbb9aaeec5e
Padman <frimpongy890@gmail.com>
2024-08-11 15:08:04 +0000

    modified api service to reference endpoint properly

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -23,9 +23,26 @@ class ApiService {
       FormData formData = FormData.fromMap({
         "file": await MultipartFile.fromFile(file.path, filename: fileName),
       });
+      // var res = await _dio.post('$BASE_URL${plantType.name.toLowerCase()}',
       var res = await _dio.post('$BASE_URL${plantType.name.toLowerCase()}',
           data: formData);
       return ApiResponse.fromJson(res.data as Map<String, dynamic>);
     } catch (e) {}
   }
 }
+//   Future<ApiResponse?> getPrediction({
+//     required File file,
+//   }) async {
+//     try {
+//       log(file.toString());
+//       log(BASE_URL);
+//       String fileName = file.path.split('/').last;
+//       FormData formData = FormData.fromMap({
+//         "file": await MultipartFile.fromFile(file.path, filename: fileName),
+//       });
+//       // var res = await _dio.post('$BASE_URL${plantType.name.toLowerCase()}',
+//       var res = await _dio.post('$BASE_URL', data: formData);
+//       return ApiResponse.fromJson(res.data as Map<String, dynamic>);
+//     } catch (e) {}
+//   }
+// }
